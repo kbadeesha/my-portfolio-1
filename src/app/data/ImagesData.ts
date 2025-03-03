cb5b7873a944304819531451ca449a1129c3c2c1
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2025-03-03 15:58:25 +0530

    added src

--- src/app/data/ImagesData.ts
+++ src/app/data/ImagesData.ts
@@ -2,6 +2,6 @@ import Header from "../../../public/assets/img/Header1.png";
 import NavBarLogo from "../../../public/assets/img/Logo.png";
 
 export const ImageData: any = {
-  HEADER: { src: Header },
-  NAVBAR_LOGO: { src: NavBarLogo },
+  HEADER: { src: Header.src },
+  NAVBAR_LOGO: { src: NavBarLogo.src },
 };
