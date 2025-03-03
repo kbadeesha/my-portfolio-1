cb693697e855d2e8abd78402cf064ac7492f35da
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2025-03-03 12:25:12 +0530

    yml changed

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/my-portfolio-1" : "",
   output: "export",
   distDir: "dist",
   images: {
