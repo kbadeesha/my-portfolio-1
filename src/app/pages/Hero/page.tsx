176f57bbea5360e91ab624ea8d0ef9de2a739bc0
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2025-03-10 09:32:33 +0530

    test - images

--- src/app/pages/Hero/page.tsx
+++ src/app/pages/Hero/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Image from "next/image"; // Import next/image
 import Experience from "@/app/components/Experience/Experience";
-import SplashCursor from "@/appcomponents/Shared/SplashCursor/SplashCursor";
+import SplashCursor from "@/app/components/SplashCursor/SplashCursor";
 // import { ImageData } from "@/app/data/ImagesData";
 
 const Hero = () => {
