21fe6a5d7fb52d807039e803d73f5700fce88905
kbadeesha <adeesha.kristhorubaduge@gmail.com>
2025-03-10 09:20:29 +0530

    test dist

---
+++ dist/_next/static/chunks/app/pages/Hero/page-f539b127cf6d2311.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[30],{6754:(e,s,_)=>{Promise.resolve().then(_.bind(_,698))}},e=>{var s=s=>e(e.s=s);e.O(0,[749,879,698,441,517,358],()=>s(6754)),_N_E=e.O()}]);
