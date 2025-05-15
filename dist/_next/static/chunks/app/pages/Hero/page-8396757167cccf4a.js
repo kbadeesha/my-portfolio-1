01f3d5c8ecfdfb840b8845c4986eb9d94794f254
kbadeesha <adeeshaxd@gmail.com>
2025-05-15 22:10:00 +0530

    Remove dist folder from repo

--- dist/_next/static/chunks/app/pages/Hero/page-8396757167cccf4a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[30],{6754:(e,s,_)=>{Promise.resolve().then(_.bind(_,2789))}},e=>{var s=s=>e(e.s=s);e.O(0,[919,879,789,441,517,358],()=>s(6754)),_N_E=e.O()}]);
