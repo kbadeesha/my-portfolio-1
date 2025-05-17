f29cfb57b041e83eb383d8efbac52623f1a60a8c
kbadeesha <adeeshaxd@gmail.com>
2025-05-17 19:48:29 +0530

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://kbadeesha.github.io/my-portfolio-1',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: './out', // Output path for static export
+};
